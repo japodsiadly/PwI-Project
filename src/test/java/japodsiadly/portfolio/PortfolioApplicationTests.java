907d754509a957b4e9c50642e73b6d42f7df3f24
Jakub Podsiadły <kubapodsiadly@gmail.com>
2022-06-10 23:22:03 +0200

    Java Spring application

---
+++ src/test/java/japodsiadly/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package japodsiadly.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
