1cb9dbcec58a22fd60d044b8e9b30493bc7218e1
Jakub Podsiadły <kubapodsiadly@gmail.com>
2022-06-19 16:49:49 +0200

    Fixed mobile scrolling

--- src/main/resources/static/js/lescroll.js
+++ src/main/resources/static/js/lescroll.js
@@ -4,36 +4,65 @@ var leScroll = function (e) {
         i = navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? !0 : !1;
     return e.scrolling = !1, e.move = function () {
         document.getElementById("container").style.top = i ? "-" + t * n + "px" : "-" + t * window.innerHeight + "px"
-    }, e.moveUp = function () {
-        0 !== t && (t--, e.move())
-    }, e.moveDown = function () {
-        t < o && (t++, e.move())
-    }, e.moveTo = function (o) {
-        t = o, e.move()
-    }, e.addEvent = function (t, o) {
-        document.querySelector(t).addEventListener("click", function () {
-            e.moveTo(o)
-        })
-    }, e.setScrollTimeout = function (t) {
-        setTimeout(function () {
-            e.scrolling = !1
-        }, t)
-    }, e
+    },
+        e.moveUp = function () {
+            0 !== t && (t--, e.move(), animationAdding(t))
+        },
+        e.moveDown = function () {
+            t < o && (t++, e.move(), animationAdding(t))
+        },
+        e.moveTo = function (o) {
+            t = o;
+            animationAdding(t);
+            e.move();
+        },
+        e.addEvent = function (t, o) {
+            document.querySelector(t).addEventListener("click", function () {
+                e.moveTo(o), animationAdding(t)
+            })
+        },
+        e.setScrollTimeout = function (t) {
+            setTimeout(function () {
+                e.scrolling = !1
+            }, t)
+        }, e
 }(leScroll || {});
 !function (e, t, o) {
     "use strict";
     for (var n = t.getElementsByTagName("li"), i = {up: 38, down: 40}, l = function (t) {
-        var o = 0, n = t || e.event;
-        n.wheelDelta ? o = (e.opera ? -1 : 1) * n.wheelDelta / 120 : n.detail && (o = -n.detail / 3), o && c(o)
-    },
+            var o = 0, n = t || e.event;
+            n.wheelDelta ? o = (e.opera ? -1 : 1) * n.wheelDelta / 120 : n.detail && (o = -n.detail / 3), o && c(o)
+        },
              c = function (e) {
-        o.scrolling || (e > 0 ? (o.scrolling = !0, o.moveUp(), o.setScrollTimeout(1500)) : 0 >= e && (o.scrolling = !0, o.moveDown(), o.setScrollTimeout(1500)))
-    }, r = function () {
-        o.moveTo(event.target.getAttribute("data-section"))
-    }, u = 0; u < n.length; u++) n[u].getAttribute("data-section") && n[u].addEventListener("click", r);
+                 o.scrolling || (e > 0 ? (o.scrolling = !0, o.moveUp(), o.setScrollTimeout(800)) : 0 >= e &&
+                     (o.scrolling = !0, o.moveDown(), o.setScrollTimeout(800)))
+             },
+             r = function () {
+                 o.moveTo(event.target.getAttribute("data-section"))
+             }, u = 0; u < n.length; u++) n[u].getAttribute("data-section") && n[u].addEventListener("click", r);
     e.addEventListener("keydown", function (e) {
         e.keyCode === i.up ? o.moveUp() : e.keyCode === i.down && o.moveDown()
-    }), e.addEventListener("resize", function () {
-        o.move()
-    }), e.addEventListener("DOMMouseScroll", l, !1), e.onmousewheel = t.onmousewheel = l
+    }),
+        e.addEventListener("resize", function () {
+            o.move()
+        }),
+        e.addEventListener("DOMMouseScroll", l, !1), e.onmousewheel = t.onmousewheel = l
 }(window, document, leScroll);
+
+function animationAdding(sectionNumber) {
+    if (parseInt(sectionNumber) === 1) {
+        if (!document.querySelector("#section1_slideRight").classList.contains("slideRight")) {
+            document.querySelector("#section1_slideRight").classList.add("slideRight");
+            document.querySelector("#section1_slideLeft").classList.add("slideLeft");
+            document.querySelector("#section1_slideDown").classList.add("slideDown");
+        } else {
+            return;
+        }
+    } else if (sectionNumber === 2) {
+
+    } else if (sectionNumber === 3) {
+
+    } else {
+        return;
+    }
+}
